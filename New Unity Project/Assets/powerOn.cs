e62a4a8fb2b6647b16461091b9ef7cf69335dbe4
Aimee Shoun <Greenaimulah@gmail.com>
2019-08-27 23:06:48 -0600

    First Scripting

---
+++ New Unity Project/Assets/powerOn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class powerOn : MonoBehaviour
+{
+
+	public int firstint = 2;
+
+	public bool firstbool = true;
+
+	public double firstdouble = 2.34;
+
+	public float firstfloat = 1.3f;
+
+	public char character = 'a';
+
+	public string firststring = "Hello World";
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
