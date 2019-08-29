3864c0b2ca2fccd37cabd9105bd9e3ead0104e65
Aimee Shoun <Greenaimulah@gmail.com>
2019-08-28 19:12:52 -0600

    Third Script
    Scripting

---
+++ New Unity Project/Assets/thirdScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class thirdScript : MonoBehaviour {
+	public string stringOne = "What up Doc?";
+		public string stringTwo = "Hows it Hanging";
+	// Use this for initialization
+	void Start ()
+	{
+		Debug.Log("stringTwo");
+		Debug.Log("Hello World!");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
