c1a723877bc74566ba427c802202ee549a584958
Aimee Shoun <10893245@my.uvu.edu>
2019-08-29 14:47:41 -0600

    Unity Practice
    Practicing

---
+++ Unity Project3/Assets/rScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rScript : MonoBehaviour
+{
+    public int firstInt = 1;
+
+    public string firstString = "Hello World!";
+
+    public char firstChar = 'a';
+
+    public bool firstBool = false;
+    public double firstDouble = 2.39475;
+
+    public float firstFloat = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
