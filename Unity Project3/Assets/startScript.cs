672895d3d7ae3b03847d265f8173795bb6c43877
Aimee Shoun <10893245@my.uvu.edu>
2019-08-29 15:39:55 -0600

    Unity Practice
    Class August 29th 2019

---
+++ Unity Project3/Assets/startScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Hello World");
+        print("I love");
+        print("coding");
+        print("I am practing!");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
