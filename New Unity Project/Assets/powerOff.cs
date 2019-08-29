9945bbe0cce0f8953aed12b8b3afbba51d4e3294
Aimee Shoun <Greenaimulah@gmail.com>
2019-08-28 19:01:14 -0600

    Second Script
    Scripting Lesson

---
+++ New Unity Project/Assets/powerOff.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class powerOff : MonoBehaviour
+{
+	public int secondInt = 1;
+
+	public char secondChar = 'b';
+
+	public float secondFloat = 1.45f;
+
+	public double secondDouble = 1.23456;
+
+	public bool secondBoolean = false;
+
+	public string secondString = "GoodBye World!";
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
