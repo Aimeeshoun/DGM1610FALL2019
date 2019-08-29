62b27c8f1c32563c9cd80e37257f3972b2c31969
Aimee Shoun <Greenaimulah@gmail.com>
2019-08-28 19:33:58 -0600

    Fourth Script
    Scripting Practice

---
+++ Scripting Project 3/Assets/scriptFour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scriptFour : MonoBehaviour
+{
+	public string myName = "Aimee";
+	// Use this for initialization
+	void Start ()
+	{
+		Debug.Log("Hello World!");
+		Debug.Log("My Name Is" + myName);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
