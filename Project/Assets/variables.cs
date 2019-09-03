b7ff09ab31fab49bcf00791355fc07e597a26147
Aimee Shoun <10893245@my.uvu.edu>
2019-09-03 15:23:16 -0600

    Learning Variables

---
+++ Project/Assets/variables.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class variables : MonoBehaviour
+    {
+        public float floatValue = 5f;
+
+        
+        public int intVaule = 20;
+
+        public string stringVaule = "Bob";
+
+        public int firePower= 32;
+        public UnityEvent Event;
+        {
+            UnityEngine.Event.Invoke();
+        }
+
+        public void OnTriggerExit(Collider other)
+        {
+            print("Hello World!");
+        }
+
+        // Start is called before the first frame update
+    // Practicing Variables
+    void Start()
+    {
+        print("Nothing");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
