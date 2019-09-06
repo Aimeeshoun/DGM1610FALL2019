37688e600c5ee3d66ad2f5bcbc7a2de52fe2e610
Aimee Shoun <greenaimulah@gmail.com>
2019-09-05 21:09:13 -0600

    Practice Variables 2

---
+++ New Unity Project (2)/Assets/NewBehaviourScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public int firstInt = 2;
+
+    public string firstString = "hello world!";
+
+    public char firstChar = 'a';
+
+    public bool firstBool = true;
+
+    public double firstDouble = 2.345324;
+
+    public float firstFloat = 2.45f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("hello how are you." + firstString);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
