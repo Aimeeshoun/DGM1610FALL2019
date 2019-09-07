dd9d31a80d3059df4eaabe3d3cb4fb512cd8a234
Aimee Shoun <greenaimulah@gmail.com>
2019-09-07 14:04:22 -0600

    I love to Script

--- New Unity Project (2)/Assets/practice1.cs
+++ New Unity Project (2)/Assets/practice1.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class practice1 : MonoBehaviour
 {
     private int firstInt = 4;
+    private double firstDouble = 2.334554445;
+    private bool firstBool = true;
 
     private string firstString = "Hello World, How are you?";
     // Start is called before the first frame update
@@ -12,6 +14,7 @@ public class practice1 : MonoBehaviour
     {
         print("Yo" + firstString);
         print( "I am here to do your bidding."+ firstInt);
+        print(  "Hello Im here!"+ firstDouble);
     }
 
     // Update is called once per frame
