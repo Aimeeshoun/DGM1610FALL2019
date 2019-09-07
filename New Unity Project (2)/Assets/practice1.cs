49d1114b6b14174f713a30d0ce6e6348e9323a5a
Aimee Shoun <greenaimulah@gmail.com>
2019-09-07 14:01:06 -0600

    Practice Makes Perfect

---
+++ New Unity Project (2)/Assets/practice1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class practice1 : MonoBehaviour
+{
+    private int firstInt = 4;
+
+    private string firstString = "Hello World, How are you?";
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Yo" + firstString);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
