7ed3557258e85e8bdfa6d2ec9fedcb927c884421
Aimee Shoun <greenaimulah@gmail.com>
2019-09-08 11:59:03 -0600

    Variable 6

---
+++ New Unity Project (2)/Assets/Variable6.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variable6 : MonoBehaviour
+{
+    public int myInt = 6;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MultiplyByTwo(myInt);
+    }
+
+    int MultiplyByTwo(int number)
+    {
+        int result;
+        result = number * 2;
+        return result;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
