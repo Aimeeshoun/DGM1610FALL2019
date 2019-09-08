47e76c79725e54326931e867e3ca813ea92e3269
Aimee Shoun <greenaimulah@gmail.com>
2019-09-08 11:52:53 -0600

    Variablefour

---
+++ New Unity Project (2)/Assets/Variablefour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variablefour : MonoBehaviour
+{
+    public int myInt = 4;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        MultiplyByTwo(myInt);
+    }
+
+    int MultiplyByTwo(int number)
+    {
+        int result;
+        result = number * 2;
+        return result;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
