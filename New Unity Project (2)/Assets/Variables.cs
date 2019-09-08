0f38423e6eee213ba018f3d0e7fac4b5e6d10120
Aimee Shoun <greenaimulah@gmail.com>
2019-09-08 11:30:08 -0600

    Variables and Functions
    Learning Variable and Functions on Youtube

---
+++ New Unity Project (2)/Assets/Variables.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+    public int firstInt = 3;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        MultiplyByTwo(firstInt);
+    }
+
+    int MultiplyByTwo(int number)
+    {
+        int result;
+        result = number * 2;
+        return result;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
