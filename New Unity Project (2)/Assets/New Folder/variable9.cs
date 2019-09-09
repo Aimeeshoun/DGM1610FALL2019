67f152edb825412338fcf17f0a1a6e3d78ae4675
Aimee Shoun <greenaimulah@gmail.com>
2019-09-09 16:37:14 -0600

    Variable 9

---
+++ New Unity Project (2)/Assets/New Folder/variable9.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class variable9 : MonoBehaviour
+{
+    private int myInt = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myInt=MultiplyByTwo(myInt);
+        Debug.Log(myInt);
+    }
+
+    int MultiplyByTwo(int number)
+    {
+        int result;
+        result = number * 2;
+        return result;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
