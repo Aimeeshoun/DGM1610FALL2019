305c54b791075ae7cc9dfdd782a34ab5fd72f1c2
Aimee Shoun <greenaimulah@gmail.com>
2019-09-10 21:15:42 -0600

    HomeWork for Sept 2 Create Event
    Create Event Collider

---
+++ New Unity Project (3)/Assets/Events/triggerEvent1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerEvent1 : MonoBehaviour
+{
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Hello World!");
+        
+    }
+}
+
