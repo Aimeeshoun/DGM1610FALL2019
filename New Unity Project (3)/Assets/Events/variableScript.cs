007fefd66a0f5a6858d68a3d5404ce14b26ce5c7
Aimee Shoun <greenaimulah@gmail.com>
2019-09-10 21:19:54 -0600

    Homework Sept 2nd Create Game Variables
    creating variables for games.

---
+++ New Unity Project (3)/Assets/Events/variableScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class variableScript : MonoBehaviour
+{
+   
+    private int firstInt = 1;
+    public bool firstBool = true;
+    public string firstString = "Hello World";
+    public char firstChar = 'a';
+    public float firstFloat = 1.2f;
+     
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
