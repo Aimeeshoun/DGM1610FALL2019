33c37f2d2a230b708d892b6f4bfe216ce3c41873
Aimee Shoun <greenaimulah@gmail.com>
2019-09-11 22:29:19 -0600

    Homework Event For Sept 2nd

--- Power Up/Assets/scripts/player2script.cs
+++ Power Up/Assets/scripts/player2script.cs
@@ -2,26 +2,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class player2script : MonoBehaviour
 
+{
+       public UnityEvent triggerEvent;
 
-{   
-    public bool enter =true;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    private void OnTriggerEnter(Collider other)
-    {
-        if (enter)
-        {
-            Debug.Log("Enter!");
-        }
-       
-       
-    }
+       private void OnTriggerEnter(Collider other)
+       {
+             triggerEvent.Invoke();
+       }
 }
+
