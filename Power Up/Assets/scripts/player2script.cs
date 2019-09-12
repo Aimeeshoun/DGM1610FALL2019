995da1080eff63d65e89ce1efb16f6492dab907e
Aimee Shoun <greenaimulah@gmail.com>
2019-09-11 22:18:02 -0600

    OnTriggerEnter

---
+++ Power Up/Assets/scripts/player2script.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player2script : MonoBehaviour
+
+
+{   
+    public bool enter =true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if (enter)
+        {
+            Debug.Log("Enter!");
+        }
+       
+       
+    }
+}
