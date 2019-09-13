f208ef9ada8ec69985c01b2755cc9ebcf8a0d5ad
Aimee Shoun <greenaimulah@gmail.com>
2019-09-12 21:57:25 -0600

    Create Event Particles

---
+++ Power Up/Assets/scripts/NewBehaviourScript.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+   public ParticleSystem particles;
+
+   private void OnMouseDown()
+   {
+       particles.Emit(100);
+   }
+}
