9e76cb197febb94fecdb116518401921b162203f
Aimee Shoun <greenaimulah@gmail.com>
2019-09-13 22:55:49 -0600

    PowerUp Health Script Homework
    I finally figured out how to add health on OntriggerEnter!

---
+++ Power Up/Assets/scripts/FloatDataScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class FloatDataScript : ScriptableObject
+{
+    public float value = 1f;
+
+    public void UpdateValue(float amount)
+    {
+        value += amount;
+    }
+}
