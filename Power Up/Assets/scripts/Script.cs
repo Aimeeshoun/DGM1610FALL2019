5a8620d098be65ec0d9b7bdfda6538719f518498
Aimee Shoun <greenaimulah@gmail.com>
2019-09-14 22:56:47 -0600

    Power Up with +5 to Health

---
+++ Power Up/Assets/scripts/Script.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script : MonoBehaviour
+{
+    public int healthValue;
+    public float Health = 100;
+    private void OnTriggerEnter(Collider other)
+    {
+        Health += 5;
+            
+        Debug.Log("Heath is"+ Health);
+    }
+}
