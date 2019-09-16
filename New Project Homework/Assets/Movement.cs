0ee778c551cdc2d0511648a2b623512caad8c2ae
Aimee Shoun <greenaimulah@gmail.com>
2019-09-15 22:50:33 -0600

    Character Movement in Game/Death

---
+++ New Project Homework/Assets/Movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float Speed = 5;
+    private Rigidbody rb;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hAxis = Input.GetAxis("Horizontal");
+        float vAxis = Input.GetAxis("Vertical");
+        
+        Vector3 movement = new Vector3(hAxis,0,vAxis) * Speed * Time.deltaTime;
+        
+        rb.MovePosition(transform.position + movement);
+    }
+}
