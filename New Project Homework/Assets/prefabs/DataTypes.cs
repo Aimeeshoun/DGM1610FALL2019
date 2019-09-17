298ce4dec9a3e92ae506db12c07814681faf259a
Aimee Shoun <greenaimulah@gmail.com>
2019-09-16 19:57:03 -0600

    DataTypes HomeWork

---
+++ New Project Homework/Assets/prefabs/DataTypes.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataTypes : MonoBehaviour
+{
+    //Characters Color 1 = red, 2 = yellow,
+    private int characterOneColor = 1;
+    private int characterTwoColor = 2;
+
+    //Character Size  1 = big, 2 = small
+    private int characterOneSize = 1;
+    private int characterTwoSize = 2;
+
+    //Character Mood
+    private bool isHappy = true;
+    private bool isSad = false;
+    
+    //Character Name
+    private string characterRedName = "Red Aimee";
+    private string characterYellowName = "Yellow John";
+
+
+} 
