3eb11ad91781c6ececdf6e5378a92a0506aa7a28
Aimee Shoun <greenaimulah@gmail.com>
2019-09-16 19:45:14 -0600

    Scriptable object CreateAssetMenu

--- New Project Homework/Assets/scripts/scriptableObject.cs
+++ New Project Homework/Assets/scripts/scriptableObject.cs
@@ -1,8 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class scriptableObject : MonoBehaviour
+[CreateAssetMenu]
+public class scriptableObject : ScriptableObject
 {
     // Start is called before the first frame update
     void Start()
