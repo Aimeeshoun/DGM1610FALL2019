f7c4aa3c95e8f5b527c34a5a86eff05053d4e079
Aimee Shoun <greenaimulah@gmail.com>
2019-09-19 21:17:54 -0600

    Character Controller Coin Pick Up Game

---
+++ New Unity Project (5)/Assets/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    
+           public float moveSpeed = 10f,  jumpSpeed = 30f, gravity = 9.81f;
+           public int jumpCount;
+           private CharacterController controller1;
+           private int jumpCountMax=2;
+           private Vector3 thePosition;
+   
+           // Start is called before the first frame update
+           void Start()
+           {
+               controller1 = GetComponent<CharacterController>();
+           }
+   
+           // Update is called once per frame
+           void Update()
+           {    
+               thePosition.y -= gravity;
+               thePosition.x = moveSpeed *Input.GetAxis("Horizontal");
+
+               if (controller1.isGrounded)
+               {
+                   thePosition.y = 0;
+                   jumpCount = 0;
+   
+               }
+           
+               if (Input.GetButtonDown("Jump") && jumpCount < jumpCountMax)
+               {
+                   thePosition.y = jumpSpeed;
+                   jumpCount++;
+               }
+           
+           
+               controller1.Move(motion:thePosition * Time.deltaTime);
+           }
+       }
+   
+   
+   
---
+++ New Unity Project (5)/Assets/destroy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class destroy : MonoBehaviour
+{
+
+    public bool enter = true;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
