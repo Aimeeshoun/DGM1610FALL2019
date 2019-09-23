b7938ce376e6ca62736353bbff07a2f1afbb6e15
Aimee Shoun <greenaimulah@gmail.com>
2019-09-22 21:22:13 -0600

    Character Controller and Coin PickUp

---
+++ CharacterController Homework/Assets/CoinPickup.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour
+{
+    public bool enter = true;
+    private float coinAmount;
+    private float amount;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            amount = coinAmount;
+            coinAmount += 1;
+            Destroy(other.gameObject);
+            Debug.Log("You have " + coinAmount + " coins!");
+        }
+    }
+}
---
+++ CharacterController Homework/Assets/scripts/Movement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed = 11;
+   public float jumpSpeed = 35f;
+   public float gravity = 9.81f;
+   private int jumpCountMax = 3;
+   private Vector3 playerPosition;
+   private CharacterController myController;
+   public int jumpCount;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        myController = GetComponent<CharacterController>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerPosition.y -= gravity;
+        playerPosition.x = moveSpeed *Input.GetAxis("Horizontal");
+
+        if (myController.isGrounded)
+        {
+            playerPosition.y = 0;
+            jumpCount = 0;
+        }
+
+        if (Input.GetButtonDown("Jump") && jumpCount < jumpCountMax)
+        {
+            playerPosition.y = jumpSpeed;
+            jumpCount++;
+        }
+
+        myController.Move(motion: playerPosition * Time.deltaTime);
+    }
+}
+
