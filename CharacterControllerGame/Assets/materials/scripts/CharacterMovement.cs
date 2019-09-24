08f632dcdc965a71db1275a50b6c539092832b09
Aimee Shoun <greenaimulah@gmail.com>
2019-09-23 18:46:06 -0600

    Game CharacterController/pickUp/ifStatement
    when the game object gets 30 coins, you win and the object disappears with particles.

---
+++ CharacterControllerGame/Assets/materials/scripts/CharacterMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterMovement : MonoBehaviour
+{
+    private CharacterController theController;
+    private int theJumpCountMax = 5;
+    private Vector3 thePlayerPosition;
+    public int theJumpCount;
+    public float gravity = 9.81f;
+    public float theJumpSpeed = 45f;
+    public float theMoveSpeed = 45f;
+    private Rigidbody _rb;
+    public float Speed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        theController = GetComponent<CharacterController>();
+   
+       
+    }
+
+ 
+// Update is called once per frame
+    void Update()
+    {
+        thePlayerPosition.y -= gravity;
+        thePlayerPosition.x = theMoveSpeed * Input.GetAxis("Horizontal");
+        thePlayerPosition.z = Input.GetAxis("Vertical");
+       
+       
+
+        if (theController.isGrounded)
+        {
+            theJumpCount = 0;
+            thePlayerPosition.y = 0;
+        }
+
+        if (Input.GetButtonDown("Jump") && theJumpCount < theJumpCountMax)
+        {
+            theJumpCount++;
+            thePlayerPosition.y = theJumpSpeed;
+        }
+
+        theController.Move(motion: thePlayerPosition * Time.deltaTime);
+    }
+}
---
+++ CharacterControllerGame/Assets/materials/scripts/CoinPickUpScript.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinPickUpScript : MonoBehaviour
+{
+    public bool enter = true;
+    private float theCoinAmount;
+    private float amountValue;
+    public ParticleSystem particles;
+    private Text myText;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            amountValue = theCoinAmount;
+            theCoinAmount += 1;
+            Destroy(other.gameObject);
+            Debug.Log("You have collected " + theCoinAmount + " coins! Congrads!");
+
+
+        }
+
+        if (amountValue == 20)
+        {
+            Debug.Log("Wow! You have 20 coins!");
+        }
+
+        if (amountValue >= 30)
+        {
+            Destroy(gameObject);
+            particles.Emit(100);
+            Debug.Log("You Won!!!!!!!!!!!!!");
+          
+
+
+        }
+
+       
+    }
+}
