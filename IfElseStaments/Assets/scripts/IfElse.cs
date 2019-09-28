d1cfadd6e5dd3082a80999ca2aced3f41ce91496
Aimee Shoun <greenaimulah@gmail.com>
2019-09-28 00:55:21 -0600

    Wrote If Else Script

---
+++ IfElseStaments/Assets/scripts/IfElse.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfElse : MonoBehaviour
+{
+    public int a = 2;
+
+    public int b = 3;
+
+    public int c = 4;
+
+    public float num = 7f;
+
+    private string myName = "Aimee";
+    private int myAge = 32;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (a - b > c)
+        {
+            print(myName);
+        }
+        else if (c + a < b)
+        {
+            print(myAge);
+        }
+
+    }
+    
+}
