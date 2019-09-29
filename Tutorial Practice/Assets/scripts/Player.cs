53e124ce95f752c1a05dd1d5f5e14a21085d85ab
Aimee Shoun <greenaimulah@gmail.com>
2019-09-28 23:41:29 -0600

    Learning Variables Private/Public/Serialize Field
    Online Tutorial

--- Tutorial Practice/Assets/scripts/Player.cs
+++ Tutorial Practice/Assets/scripts/Player.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    //public or private ref
+    //data type (int, float, bool, string)
+    //every variable has a name
+    //opitional value assigned
+    //other objects can change a public variable
+    // ex: when mario pick up power up and it changes its speed
+    public float speed = 3.5f;
+    // underscore to show its private
+   //serializeField allows private var to be changed in inpector but cannot be changed by gameobjects and scripts
+   [SerializeField]
+   private float _exampleFloat;
+    
+    
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +33,6 @@ public class Player : MonoBehaviour
         //new Vector3 (1, 0,0}
         //converting 1 meter for frame to 1 meter per second, Time.deltaTime = Real time
         //move 5 meters per second, all vectors ( 5,0,0 ) gets multiplied by 5
-        transform.Translate(Vector3.right * 5 * Time.deltaTime);
+        transform.Translate(Vector3.right * speed * Time.deltaTime);
     }
 }
