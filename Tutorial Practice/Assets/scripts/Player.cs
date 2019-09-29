b28da8bab5e93f476fb8b7c5ad75c3b5917e0af9
Aimee Shoun <greenaimulah@gmail.com>
2019-09-28 23:16:09 -0600

    Learning Transform.Translate.Vector3 right/left
    Online Tutorial

---
+++ Tutorial Practice/Assets/scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //take the current position = new postition (0,0,0)
+        transform.position = new Vector3(0,0,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {  //moving character to the right
+        //i could also use Vector3(right)
+        // Vector3 (-1,0,0) or (left) moves player to left
+        transform.Translate(Vector3(1.0,0));
+    }
+}
