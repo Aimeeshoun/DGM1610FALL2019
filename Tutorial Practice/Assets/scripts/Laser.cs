dcceb081d48166e77ab3b731edbf3bfd63ae545b
Aimee Shoun <greenaimulah@gmail.com>
2019-09-29 19:34:19 -0600

    Learning how to Shoot Laser Out of Object
    Instantiate GameObjects Online Tutorial

---
+++ Tutorial Practice/Assets/scripts/Laser.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    private float _speed = 8;
+    
+
+    //speed variable of 8
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //translate laser up
+        
+        transform.Translate(Vector3.up*Time.deltaTime);
+
+        
+    }
+}
