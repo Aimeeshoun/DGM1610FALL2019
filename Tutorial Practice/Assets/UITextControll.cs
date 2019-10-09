116fe9f6dc7d7e42e02ef15f5d2c9751049e2339
Aimee Shoun <greenaimulah@gmail.com>
2019-10-08 23:15:22 -0600

    Experimenting with UI Text & OntriggerEnter
    Not currently working the way I would like. I will finish tomorrow. Saving Progress.

---
+++ Tutorial Practice/Assets/UITextControll.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Mime;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITextControll : MonoBehaviour
+{
+
+    public Text textMy;
+   
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Instantiate(textMy);
+
+    }
+
+}
