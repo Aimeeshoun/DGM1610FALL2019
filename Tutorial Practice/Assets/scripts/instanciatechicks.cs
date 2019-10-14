0dcb985a43185b2b2cbf6e219bf12e0f71475d8f
Aimee Shoun <greenaimulah@gmail.com>
2019-10-14 17:49:01 -0600

    Instantiate Little Chick from Enemy Chicken KeyDown.LeftShift Scripts

---
+++ Tutorial Practice/Assets/scripts/instanciatechicks.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class instanciatechicks : MonoBehaviour
+{
+     
+        [SerializeField]
+        private float _speed = 3.5f;
+        [SerializeField]
+        private GameObject _laserPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+                {
+                    Instantiate(_laserPrefab, new Vector3(transform.position.x,transform.position.y-.4f,0), Quaternion.identity);
+                }
+    }
+}
