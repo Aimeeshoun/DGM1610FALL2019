06b1ee4bb4978b4747d759bc7a8b2900668bc8ff
Aimee Shoun <greenaimulah@gmail.com>
2019-10-14 19:13:29 -0600

    Move Script for Player, Added Gem Assets w/ Box Colliders

---
+++ Tutorial Practice/Assets/scripts/MoveMeScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveMeScript : MonoBehaviour
+{
+    public float hortizontalInput;
+    public float verticalInput;
+    [SerializeField] private float _speed = 3.5f;
+    [SerializeField] private GameObject _laserPrefab;
+    private Vector3 thePosition;
+    private CharacterController controller;
+    public float gravity = 9.81f;
+    public float jumpSpeed = 3f;
+    private int theJumpCount;
+    public int theJumpCountMax = 4;
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+       
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(_laserPrefab, new Vector3(transform.position.x, transform.position.y + 1.5f, 0),
+                Quaternion.identity);
+            controller = GetComponent<CharacterController>();
+        }
+
+    }
+
+    void Update()
+        {
+            thePosition.x = _speed * Input.GetAxis("Horizontal");
+            thePosition.y -= gravity;
+
+            if (controller.isGrounded)
+            {
+                thePosition.y = 0;
+                theJumpCount = 0;
+            }
+
+            if (Input.GetButtonDown("Jump") && theJumpCount < theJumpCountMax)
+            {
+                thePosition.y = jumpSpeed;
+                theJumpCount++;
+            }
+
+            controller.Move(thePosition * Time.deltaTime);
+        }
+
+    }
+
+    
+
