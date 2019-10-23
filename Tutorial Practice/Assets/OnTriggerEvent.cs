9c55ebbb1be13d324a912973323bb57605ba8d09
John <10024624@my.uvu.edu>
2019-10-23 06:31:29 -0600

    Fixed HealthBar /Image UI

---
+++ Tutorial Practice/Assets/OnTriggerEvent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider))]
+public class OnTriggerEvent : MonoBehaviour
+{
+    private void Awake()
+    {
+        GetComponent<Collider>().isTrigger = true;
+    }
+
+    public UnityEvent triggerEvent;
+    private void OnTriggerEnter(Collider other)
+    {
+        triggerEvent.Invoke();
+    }
+}
