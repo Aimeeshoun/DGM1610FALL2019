26383c11f0566e99c4d064a64008abeb64b1dc5b
Aimee <10893245@my.uvu.edu>
2019-10-25 23:06:24 -0600

    Collection Looping Script

--- Tutorial Practice/Assets/Myscripts/CandyData.cs
+++ Tutorial Practice/Assets/Myscripts/CandyData.cs
@@ -1,10 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+
 [CreateAssetMenu]
 public class CandyData : GameArtData
 {
-      public string candyType;
-      
+    public string candyType;
 
+    public List<GameArtData> theCollectionList;
+
+    public void AddData(GameArtData obj)
+    {
+        
+        if (obj.collected)
+        {
+            Debug.Log(candyType);
+        }
+
+        if (!theCollectionList.Contains(obj))
+        {
+            theCollectionList.Add(obj);
+
+        }
+
+        if (theCollectionList.Contains(obj)) ;
+
+        {
+            theCollectionList.Remove(obj);
+        }
+    }
+
+
+    public void RemoveLastItem()
+    {
+        theCollectionList.RemoveAt(theCollectionList.Count - 1);
+    }
+
+
+    
 }
