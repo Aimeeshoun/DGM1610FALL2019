426201f71f254289dbf3339f5feae930ecb36774
Aimee <10893245@my.uvu.edu>
2019-11-12 15:21:59 -0700

    Coroutines Script InClass

---
+++ Tutorial Practice/Assets/Coroutines4.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coroutines4 : MonoBehaviour
+{
+    private int i = 1000;
+    private WaitForSeconds wfsOBJ = new WaitForSeconds(1f);
+
+    IEnumerator Start()
+    {
+        while (i > 0)
+        {
+            Debug.Log("Hi");
+            yield return wfsOBJ;
+            i--;
+        }
+
+        Debug.Log(message: "Go!");
+    }
+}
