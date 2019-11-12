725935eb27cf87a99164eccaaafdbdff016ecb87
Aimee <10893245@my.uvu.edu>
2019-11-12 15:04:24 -0700

    Working on Enum Switch, Coroutines In Class Work

---
+++ Tutorial Practice/Assets/Debug Homework/Coroutines.cs
@@ -0,0 +1,50 @@
+﻿
+using System.Diagnostics.Tracing;
+using System.Globalization;
+using UnityEngine;
+
+public class Coroutines : MonoBehaviour
+{
+   // IEnumerator Start()
+   // {
+     //   yield return, new WaitForSeconds(1f);
+     //   Debug.Log(3);
+     //   yield return, new WaitForSeconds(1f);
+     //   Debug.Log(2);
+     //   yield return, new WaitForSeconds(1f);
+     //   Debug.Log(1);
+     //   yield return, new WaitForSeconds(1f);
+     //   Debug.Log("Go!");
+
+
+    }
+    
+    
+   // private void Start()
+ //   {
+        //StartLater();
+    //    Invoke(nameof(StartLater), time:3f);
+     //   Invoke(nameof(Counter), time:1f);
+    //    Invoke(nameof(Counter2),time: 2f);
+    //    Invoke(nameof(Counter3), time: 3f);
+   // }
+
+   // void StartLater()
+  //  {
+  //      Debug.Log("3");
+        
+ //   }
+
+  //  void Counter()
+     //    {
+       //      Debug.Log("1");
+   //      }
+  //  void Counter2()
+  //  {
+  //      Debug.Log("2");
+  //  }
+//    void Counter3()
+  //  {
+  //      Debug.Log("3");
+  //  }
+//}
---
+++ Tutorial Practice/Assets/Debug Homework/IntBullet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+    [CreateAssetMenu]
+    public class intBullet : WeaponData
+    {
+   
+    
+        public void Instanciate(GameObject weapon)
+        {
+
+            Instantiate(weapon, new Vector3(0, 0, 0), Quaternion.identity);
+
+        }
+
+      
+    }
+
+
