b33e07fb698600fbcef964bf46ec4c488a18a352
Aimee <10893245@my.uvu.edu>
2019-11-12 15:10:10 -0700

    Coroutines InClass /Delay , Run, Repeat

---
+++ Tutorial Practice/Assets/Coroutines2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coroutines2 : MonoBehaviour
+{
+   private int i = 3;
+
+   IEnumerator Start()
+   {
+      while (i > 0)
+      {
+         yield return new WaitForSeconds(1f);
+         Debug.Log(i);
+         i--;
+      }
+      yield return new WaitForSeconds(1f);
+      Debug.Log("Go!");
+   }
+   
+}
