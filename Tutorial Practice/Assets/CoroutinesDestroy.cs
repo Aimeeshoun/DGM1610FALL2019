e847464e95efcfaeb8062e396594266e6fbb91b4
Aimee <10893245@my.uvu.edu>
2019-11-14 20:37:34 -0700

    Destroy Player Coroutin when TriggerEnter Water

---
+++ Tutorial Practice/Assets/CoroutinesDestroy.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CoroutinesDestroy : MonoBehaviour
+{
+    public bool canRun = true;
+    //  public int index = 3;
+   // public IntData index;
+    public float seconds = 1f;
+    private WaitForSeconds wfsobj;
+    
+
+    public void Run()
+    {
+        StartCoroutine(OnRun());
+        wfsobj = new WaitForSeconds(seconds);
+    }
+    // Start is called before the first frame update
+    //IEnumerator Start()
+   
+    //Colliders ( detect mouse down)
+    //Ontriggers use Events
+    IEnumerator OnRun()
+    { //if statments do once, While repeats
+        //use : while(true) if you do not have a canRun bool
+        while (canRun)
+        //while (index. > 0 )
+       // while (index.value > 0 )
+        {
+           // index.value--;
+            //  index--;
+            //  yield return new WaitForSeconds(3f);
+            yield return wfsobj;
+            Destroy(this.gameObject);
+            // Debug.Log(index);
+            Debug.Log("hi");
+
+        }
+
+        Debug.Log("Run");
+    }
+}
+
+
---
+++ Tutorial Practice/Assets/CoroutinesUitext.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoroutinesUitext : MonoBehaviour
+{
+
+    public bool canRun = true;
+    //  public int index = 3;
+    public IntData index;
+    public float seconds = 1f;
+    private WaitForSeconds wfsobj;
+
+    public void Run()
+    {
+        StartCoroutine(OnRun());
+        wfsobj = new WaitForSeconds(seconds);
+    
+    // Start is called before the first frame update
+    //IEnumerator Start()
+   
+    //Colliders ( detect mouse down)
+    //Ontriggers use Events
+    IEnumerator OnRun()
+    { //if statments do once, While repeats
+        //use : while(true) if you do not have a canRun bool
+        //while (canRun)
+        //while (index. > 0 )
+        while (index.value > 0 )
+        {
+            index.value--;
+            //  index--;
+            //  yield return new WaitForSeconds(3f);
+            yield return wfsobj;
+            Debug.Log(index);
+            
+        }
+
+        //Debug.Log("Run");
+    }
+}
+
+
+
+}
