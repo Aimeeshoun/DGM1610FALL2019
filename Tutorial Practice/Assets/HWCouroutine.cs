16cf78f3994c00a814b1a9e1c9099e3027853ef7
Aimee <10893245@my.uvu.edu>
2019-11-19 14:53:35 -0700

    Working on UI Text Countdown

---
+++ Tutorial Practice/Assets/HWCouroutine.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HWCouroutine : MonoBehaviour
+{
+
+    public int timeLeft = 3;
+    public Text countdown;
+
+    void Start()
+    {
+        StartCoroutine("LoseTime");
+        Time.timeScale = 1;
+    }
+
+    void Update()
+    {
+        if (timeLeft >= 1)
+        {
+            countdown.text = ("" + timeLeft);
+        }
+        else
+        {
+            countdown.text = (("Ready Set, Go!"));
+        }
+
+                if (timeLeft <= -1)
+                {
+                    countdown.text = ("");
+                }
+            }
+
+            IEnumerator LostTime()
+            {
+                while (timeLeft > -1)
+                {
+                    yield return new WaitForSeconds(1);
+                    timeLeft--;
+                }
+
+                while (timeLeft == 1)
+                {
+                    yield return new WaitForSeconds(1);
+                    countdown.text = ("test");
+
+                }
+            }
+
+        }
+   
