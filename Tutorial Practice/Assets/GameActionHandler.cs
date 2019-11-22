72bb188b8fbb56605a38433c9bbcb9ef558bff59
Aimee <10893245@my.uvu.edu>
2019-11-21 20:44:31 -0700

    Animation and Nav Mesh Agent Enemy Bats

---
+++ Tutorial Practice/Assets/GameActionHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameActionHandler : MonoBehaviour
+{
+ //   public UnityEvent handlerEvent;
+    //public GameAction theGameActionObject;
+
+  //  void Start()
+//    {
+  //      theGameActionObject.action = RaisingTheHandler;
+    }
+
+   // public void RaisingTheHandler();
+  //  {
+   //     handlerEvent.Invoke();
+   // }
+//}
---
+++ Tutorial Practice/Assets/InputButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class InputButton : MonoBehaviour
+{
+    public string MyInputWord;
+    public UnityEvent MyInputEvent;
+
+    void Update()
+
+    {
+
+        if (Input.GetButtonDown(MyInputWord))
+
+        {
+
+            MyInputEvent.Invoke();
+
+        }
+
+    }
+
+}
+
