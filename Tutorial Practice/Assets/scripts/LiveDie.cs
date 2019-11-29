1560e5e6f35e67a128d3e8a51666cb9c04c894e7
Aimee <10893245@my.uvu.edu>
2019-11-29 10:35:20 -0700

    Else If Statement Practice

---
+++ Tutorial Practice/Assets/scripts/LiveDie.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiveDie : MonoBehaviour
+{
+    private int life = 100;
+    void Start()
+    {
+        life = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(life <1)
+        {
+            Debug.Log("Game Over!");
+           
+        }
+        else if (life < 20)
+        {
+            Debug.Log("Dying!");
+        }
+       
+        else
+
+        {
+            Debug.Log("Playing");
+        }
+    }
+}
