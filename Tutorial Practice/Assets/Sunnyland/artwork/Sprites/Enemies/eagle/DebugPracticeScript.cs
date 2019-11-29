3fa5bc76cd45abf239392a16e2b0ba478d402029
Aimee <10893245@my.uvu.edu>
2019-11-29 09:33:27 -0700

    Reviewing Functions and Writing Scripts, Asset Import

---
+++ Tutorial Practice/Assets/Sunnyland/artwork/Sprites/Enemies/eagle/DebugPracticeScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugPracticeScript : MonoBehaviour
+{
+ void Awake()
+ {
+
+ }
+ void Start()
+ {
+  Shoot(10); 
+ }
+
+ void Update()
+ {
+ }
+
+ void Shoot(int data)
+  {
+   Debug.Log("Shooting");
+   Debug.Log(data);
+
+  }
+ }
+
