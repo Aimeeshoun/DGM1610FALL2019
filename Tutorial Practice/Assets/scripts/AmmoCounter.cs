2aa4ce8855a548fb23673cdb254f8ba434445195
Aimee <10893245@my.uvu.edu>
2019-11-29 13:33:28 -0700

    Got Bullet Counter UI Controller to work

--- Tutorial Practice/Assets/scripts/AmmoCounter.cs
+++ Tutorial Practice/Assets/scripts/AmmoCounter.cs
@@ -1,22 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.IO.MemoryMappedFiles;
-using UnityEngine;
-using UnityEngine.Experimental.PlayerLoop;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 [RequireComponent(typeof(Text))]
 public class AmmoCounter : MonoBehaviour
 {
 
-    public int ammo = 30;
-    public Text theUItext;
+   
     public int numberofammo;
     public Text SecUItext;
 
     void Start()
     {
-        theUItext = GetComponent<Text>();
+   
         SecUItext = GetComponent<Text>();
 
     }
@@ -24,23 +19,24 @@ public class AmmoCounter : MonoBehaviour
     void Update()
     {
 
-        if (theUItext != null)
+        if (SecUItext != null)
         {
-            theUItext.text = ammo.ToString();
+          
             SecUItext.text = numberofammo.ToString();
             if (Input.GetKeyDown(KeyCode.RightShift))
             {
-                ammo -= 1;
+               
                 numberofammo += 1;
                 ;
             }
 
             Debug.Log(numberofammo);
+           
         }
 
         void UpdateText(IntData data)
         {
-            theUItext.text = data.value.ToString();
+         
             SecUItext.text = data.value.ToString();
         }
 
---
+++ Tutorial Practice/Assets/scripts/ammocounter2.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+[RequireComponent(typeof(Text))]
+public class ammocounter2 : MonoBehaviour
+
+    {
+
+        public int ammo = 30;
+        public Text theUItext;
+
+
+        void Start()
+        {
+            theUItext = GetComponent<Text>();
+
+
+        }
+
+        void Update()
+        {
+
+            if (theUItext != null)
+            {
+                theUItext.text = ammo.ToString();
+
+                if (Input.GetKeyDown(KeyCode.RightShift))
+                {
+                    ammo -= 1;
+
+                    ;
+                }
+
+
+                Debug.Log(ammo);
+            }
+
+            void UpdateText(IntData data)
+            {
+                theUItext.text = data.value.ToString();
+
+            }
+        }
+    }
+
