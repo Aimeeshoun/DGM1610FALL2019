e5ce67a09f15a39d63921ce4ac9b0c56f17ab55e
Aimee <10893245@my.uvu.edu>
2019-12-01 12:15:36 -0700

    Created Power Down/powerUp ImageUI Healthbar

--- Tutorial Practice/Assets/Myscripts/ImageControl.cs
+++ Tutorial Practice/Assets/Myscripts/ImageControl.cs
@@ -1,17 +1,30 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
-
 [RequireComponent(typeof(Image))]
 public class ImageControl : MonoBehaviour
+
 {
+
     private Image img;
+
     void Awake()
+
     {
+
         img = GetComponent<Image>();
+
     }
 
+
+
     public void UpdateImage(FloatData data)
+
     {
+
         img.fillAmount = data.value;
+
     }
+
 }
