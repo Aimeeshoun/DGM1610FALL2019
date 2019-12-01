acd786955f81bf68c15d9605716b21263e6fee61
Aimee <10893245@my.uvu.edu>
2019-12-01 13:44:41 -0700

    Experimenting with if Statements

---
+++ Tutorial Practice/Assets/Myscripts/CountGameObjects.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CountGameObjects : MonoBehaviour
+{
+    public int bulletCount = 30;
+    [SerializeField] public GameObject gameobject;
+    
+    
+    
+    void Start()
+    {
+        bulletCount =   GameObject.FindGameObjectsWithTag("Bullet").Length;
+        Debug.Log(bulletCount);
+    }
+  
+    // Update is called once per frame
+    void Update()
+    {
+
+       
+        
+        
+        
+        void CountGameObject(GameObject gameobject)
+        {
+            
+            if (bulletCount < 100)
+                {
+                    if (Input.GetKeyDown(KeyCode.RightShift))
+                    {
+
+                        Instantiate(gameobject, new Vector3(transform.position.x, transform.position.y, transform.position.z),
+                            Quaternion.identity);
+                        Debug.Log(bulletCount);
+            }
+            else if(bulletCount > 100)
+                    {
+                        Destroy(gameobject);
+
+                    }
+            }
+                
+        }
+    }
+}
---
+++ Tutorial Practice/Assets/PrefabController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+public class PrefabController : MonoBehaviour
+{
+
+    public int weaponCount;
+    public GameObject weapon1, weapon2, weapon3,weapon4,weapon5,weapon6,weapon7;
+    
+   
+    void Start()
+    {
+        weaponCount = GameObject.FindGameObjectsWithTag("Weapon").Length;
+      
+
+
+    }
+
+    void Update()
+    {
+        if (weapon1)
+        {
+            if (weaponCount > 5)
+            {
+                Destroy(weapon1);
+            }
+        }if (weapon2)
+        { if (weaponCount > 5)
+            {
+                Destroy(weapon2);
+            }
+            
+        }if (weapon3)
+        { if (weaponCount > 5)
+            {
+                Destroy(weapon3);
+            }
+            
+        }if (weapon4)
+        {
+            if (weaponCount > 5)
+            {
+                Destroy(weapon4);
+            }
+        }if (weapon5)
+        {
+            if (weaponCount > 5)
+            {
+                Destroy(weapon5);
+            }
+        }if (weapon6)
+        {
+            if (weaponCount > 5)
+            {
+                Destroy(weapon6);
+            }
+        }
+    }
+}
