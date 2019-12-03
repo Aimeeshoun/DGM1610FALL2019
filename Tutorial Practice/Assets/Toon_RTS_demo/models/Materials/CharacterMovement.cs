dae1db5726a7577ad710fd6a9f9b06f1c4d185bb
Aimee <10893245@my.uvu.edu>
2019-12-03 10:34:52 -0700

    Experimenting with Player Movement

---
+++ Tutorial Practice/Assets/Toon_RTS_demo/models/Materials/CharacterMovement.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+
+    public CharacterController controller;
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    private bool jump = false;
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+
+    private Vector3 moveDirection = Vector3.zero;
+
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+
+        if (controller.isGrounded)
+        {
+            // We are grounded, so recalculate
+            // move direction directly from axes
+
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            moveDirection *= speed; if (Input.GetKey(KeyCode.Space))
+
+        {
+            jump = true;
+            moveDirection.y = jumpSpeed;
+            
+        }
+    }
+    moveDirection.y -= gravity * Time.deltaTime;
+    
+
+    // Move the controller
+    controller.Move(moveDirection * Time.deltaTime);
+    }
+    void FixedUpdate()
+    {
+       
+        jump = false;
+    }
+}
