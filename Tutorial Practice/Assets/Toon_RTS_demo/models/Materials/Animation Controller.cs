2250a25c248819c91cae655c53743e7052d10db9
Aimee <10893245@my.uvu.edu>
2019-12-03 20:10:43 -0700

    Animation Controller Script

---
+++ Tutorial Practice/Assets/Toon_RTS_demo/models/Materials/Animation Controller.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Animator))]
+
+public class AnimationController : MonoBehaviour
+{
+    public  Animator animator;
+
+        void Start()
+        {
+
+            animator = GetComponent<Animator>();
+        }
+
+        void Update()
+        {
+       
+            if (Input.GetKey(KeyCode.RightArrow))
+            {animator.ResetTrigger("Runing");
+                animator.SetTrigger("Walking");
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+               animator.ResetTrigger("Walking");
+                animator.SetTrigger("Running");
+            }
+
+            animator.SetTrigger("Idle");
+
+            {
+                
+            }
+        }
+    }
