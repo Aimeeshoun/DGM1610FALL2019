db7b2f8fc055ed76a94704c7d4612633805b5797
Aimee <10893245@my.uvu.edu>
2019-12-03 19:19:47 -0700

    Array And List Collections

---
+++ Tutorial Practice/Assets/Toon_RTS_demo/models/Materials/ArrayList.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ArrayList : ScriptableObject
+{
+
+    public List<int> intList;
+    public List<string> stringList;
+    public List<float> floatList;
+    public List<FloatData> floatDataList;
+    public List<GameObject> gameObjectList;
+}
---
+++ Tutorial Practice/Assets/Toon_RTS_demo/models/Materials/ArrayListExp2.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class ArrayListExp2 : ScriptableObject
+{
+    public List<GameObject> GameObjectsList;
+    public List<WeaponData> WeaponDataList; 
+
+}
