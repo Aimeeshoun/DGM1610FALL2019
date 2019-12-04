f22c4d94c34c4b96b9b33379dcd788f6e6ac9ce8
Aimee <10893245@my.uvu.edu>
2019-12-03 19:38:36 -0700

    Collection Loop Practice

---
+++ Tutorial Practice/Assets/Toon_RTS_demo/models/Materials/Loops.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+
+public class Loops : ScriptableObject
+
+{
+
+    public List<WeaponData> collectionList;
+
+
+
+    public void AddTheData(WeaponData obj)
+
+    {
+
+        if (!collectionList.Contains(obj))
+
+        {
+
+            collectionList.Add(obj);
+
+        }
+
+    }
+
+
+
+    public void RemoveTheLastObject()
+
+    {
+
+        if (collectionList.Capacity != 0)
+
+        {
+
+            collectionList.RemoveAt(collectionList.Count-1);
+            
+
+        }
+
+    }
+
+}
+
