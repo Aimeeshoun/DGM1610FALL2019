ec0be393cd40663e4d54255f729dedd544e53348
Aimee <10893245@my.uvu.edu>
2019-12-06 15:17:12 -0700

    Experimenting Writing Animation Scripts

---
+++ Tutorial Practice/Assets/Practrice.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AnimationController))]
+public class Practrice : MonoBehaviour
+{
+    public Animation animator;
+    public bool isrunning;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        while (true)
+        {
+            animator.Play("running");
+        }
+    }
+}
