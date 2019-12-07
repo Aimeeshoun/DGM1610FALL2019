e642a4a7d41122e0a303cccfbcb3cc0e2bdff670
Aimee <10893245@my.uvu.edu>
2019-12-07 12:32:44 -0700

    Experimenting with PowerUps and Coroutines

---
+++ Tutorial Practice/Assets/CoroutinePowerUp.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoroutinePowerUp : MonoBehaviour
+{
+
+    public GameObject gameObject;
+    public bool powerUpActivate = true;
+
+    public void Start()
+    {
+        StartCoroutine((OnGo()));
+    }
+
+    IEnumerator OnGo()
+    {
+       while(powerUpActivate){
+           if (gameObject.CompareTag("Potion"))
+           
+            Destroy(this.gameObject, 1f);}
+        yield return new WaitForSeconds(1f);
+        Instantiate(gameObject, transform.position + new Vector3(0, 0, 0), Quaternion.identity);
+       
+    }
+}
+   
