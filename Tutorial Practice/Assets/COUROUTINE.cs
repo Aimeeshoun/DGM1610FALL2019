366b12d17ab967022ce6d8b8cd2b2e935f4d34ad
Aimee <10893245@my.uvu.edu>
2019-12-06 19:13:24 -0700

    Coroutine Practice

---
+++ Tutorial Practice/Assets/COUROUTINE.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class COUROUTINE : MonoBehaviour
+{
+
+    public GameObject Prefab;
+
+   void Update()
+    {
+        StartCoroutine(DIE());
+    }
+
+    IEnumerator DIE()
+    {
+        {
+Destroy(gameObject,3f);
+        }
+        yield return new WaitForSeconds(1);
+    }
+}
