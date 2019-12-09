93c1d4747dd5d61990ed1056a2febc57a6c55717
Aimee <10893245@my.uvu.edu>
2019-12-09 12:34:46 -0700

    AnimationContollerTrigger

---
+++ Tutorial Practice/Assets/AnimationTriggerScript.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationTriggerScript : MonoBehaviour
+{
+   private void Update()
+   {
+      void OnTriggerEnter()
+      {
+
+         if (CompareTag("Enemy"))
+         {
+            gameObject.GetComponent<Animator>().Play("Attack");
+            Destroy(gameObject);
+
+         }
+
+         void OnTriggerExit()
+         {
+            if (CompareTag("Enemy"))
+            {
+               gameObject.GetComponent<Animator>().Play("Attack");
+               Destroy(gameObject);
+            }
+         }
+      }
+   }
+}
