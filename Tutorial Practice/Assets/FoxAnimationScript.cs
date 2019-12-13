5da5437970182730e9ffd294ce96ce61cc448850
Aimee <10893245@my.uvu.edu>
2019-12-13 10:48:57 -0700

    Animation Controller Script Using Bool, Experimenting with Animation

---
+++ Tutorial Practice/Assets/FoxAnimationScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoxAnimationScript : MonoBehaviour
+{
+    private Animator foxAnima;
+    private bool isMoving;
+    void Start()
+    {
+        foxAnima = gameObject.GetComponent<Animator>();
+        isMoving = false;
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            isMoving = true;;
+           
+        }
+        else isMoving = false;
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            isMoving = true;
+        }
+        else isMoving = false;
+
+        if (isMoving == false)
+        {
+            foxAnima.SetBool("isMoving", false);
+        }
+
+        if (foxAnima == true)
+        {
+            foxAnima.SetBool("isMoving",true);
+        }
+
+    }
+}
