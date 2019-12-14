cedf6b4311a5bf2fcaddc11b79a09d4a09541f7d
Aimee <10893245@my.uvu.edu>
2019-12-13 20:31:39 -0700

    Enum Switch Experimenting, Learning Weapon Switch

---
+++ Tutorial Practice/Assets/Myweapons/enumSwitchPracticeScript.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using UnityEngine;
+
+public class enumSwitchPracticeScript : MonoBehaviour
+{
+    
+    public GameObject prefab;
+
+    public enum WeaponType
+    {
+        Sword,
+        Swordone,
+        Swordtwo
+    }
+
+    public WeaponType weaponType;
+
+    public string[] arrayString;
+
+    private void Start()
+    {
+      //  if (weaponType == WeaponType.Sword)
+        {
+     //      Instantiate (prefab,new Vector3(0,0,0),Quaternion.identity);
+        }
+     //   else if(weaponType == WeaponType.Swordone)
+        {
+     //       Instantiate(prefab, new Vector3(0, 0, 0), Quaternion.identity);
+        }
+    //    else if (weaponType == WeaponType.Swordtwo)
+        {
+     //       Instantiate(prefab, new Vector3(0,0,0),Quaternion.identity);
+        }
+        arrayString = new String[5];
+        arrayString[0] = "Sword";
+        arrayString[1] = "Potion";
+        
+        for (int i = 0; i < arrayString.Length; i++)
+        {
+            print(arrayString[i]);
+        }
+
+        void Update()
+        {
+            switch (weaponType)
+            {
+                case WeaponType.Sword:
+                    Instantiate(prefab, new Vector3(0, 0, 0), Quaternion.identity);
+                    break;
+                case WeaponType.Swordone:
+                    Instantiate(prefab, new Vector3(0, 0, 0), Quaternion.identity);
+                    break;
+                case WeaponType.Swordtwo:
+                    Instantiate(prefab, new Vector3(0, 0, 0), Quaternion.identity);
+                    break;
+                default:
+                    Instantiate(prefab, new Vector3(0, 0, 0), Quaternion.identity);
+                    break;
+
+            }
+        }
+    }
+}
