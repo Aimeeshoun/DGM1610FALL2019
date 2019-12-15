b07387768bedb25a1f18794c1012ac34a7e9e8fa
Aimee <10893245@my.uvu.edu>
2019-12-14 18:51:49 -0700

    Experimenting with Coroutines

---
+++ Tutorial Practice/Assets/CoroutinePracticethree.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CoroutinePracticethree : MonoBehaviour
+{
+
+    public UnityEvent startSpinningEvent, keepSpinningEvent, stopSpinningEvent;
+    public float seconds = 5f;
+    public int counter = 5;
+    public bool isSpinning;
+
+
+    private WaitForSeconds wfsObj;
+
+    private void Awake()
+    {
+        wfsObj = new WaitForSeconds(seconds);
+    }
+
+    public void CallCoroutine()
+    {
+        StartCoroutine(RunCoroutine());
+    }
+
+    IEnumerator RunCoroutine()
+
+    {
+        startSpinningEvent.Invoke();
+
+        while (counter > 0)
+        {
+            keepSpinningEvent.Invoke();
+            yield return wfsObj;
+            counter--;
+        }
+
+        while (isSpinning)
+
+        {
+            keepSpinningEvent.Invoke();
+            yield return wfsObj;
+            counter--;
+        }
+
+
+        stopSpinningEvent.Invoke();
+
+    }
+}
---
+++ Tutorial Practice/Assets/OnTriggerExit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+[RequireComponent(typeof(BoxCollider))]
+public class OnTriggerExit : MonoBehaviour
+
+{
+    
+        private void Awake()
+        {
+            GetComponent<BoxCollider>().isTrigger = false;
+        }
+
+        public UnityEvent triggerEvent;
+
+        private void OnTrigger(Collider other)
+        {
+            triggerEvent.Invoke();
+        }
+    }
+
