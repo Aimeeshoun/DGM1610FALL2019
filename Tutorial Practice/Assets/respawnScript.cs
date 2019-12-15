89064129d6816a1550cdae14330e3295beb8b29c
Aimee <10893245@my.uvu.edu>
2019-12-15 14:45:39 -0700

    Respawn Attempt three using Coroutine

--- Tutorial Practice/Assets/respawnScript.cs
+++ Tutorial Practice/Assets/respawnScript.cs
@@ -1,19 +1,35 @@
 ﻿
+using System.Collections;
+using UnityEditor.Experimental.UIElements.GraphView;
 using UnityEngine;
 
 public class respawnScript : MonoBehaviour
 {
     public GameObject spawnPoint;
     public GameObject Player = GameObject.FindGameObjectWithTag("myPlayer");
-   
-        void OnTriggerEnter (Collider col)
+    public GameObject CurrentCheckPoint;
+    private GameObject myPlayer;
+    public GameObject myPrefab;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (gameObject.CompareTag("myPlayer"))
         {
-            if ( gameObject.CompareTag( "myPlayer"))
-            {
-                Destroy(gameObject, 3f); 
-                GameObject newPlayer= Instantiate(Player, new Vector2(0,0), Quaternion.identity);
-                
-            }
+            StartCoroutine(RespawnPlayerCo());
+            Invoke("DelayedSpawn", 2.5f);
+
         }
     }
- 
+
+    public IEnumerator RespawnPlayerCo()
+    {
+        {
+            Debug.Log("dead");
+            Destroy(gameObject, 3f);
+            yield return new WaitForSeconds(5);
+            Debug.Log("respawn");
+            GameObject newPlayer = Instantiate(Player, new Vector2(0, 0), Quaternion.identity);
+            GameObject player = Instantiate<GameObject>(myPrefab);
+        }
+    }
+}
