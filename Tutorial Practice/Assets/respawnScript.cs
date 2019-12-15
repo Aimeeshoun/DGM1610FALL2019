6113cf6a73eded1cfc2c7c40257351c3d7018408
Aimee <10893245@my.uvu.edu>
2019-12-15 10:45:08 -0700

    Trying to figure out How to Re spawn Player

---
+++ Tutorial Practice/Assets/respawnScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class respawnScript : MonoBehaviour
+{
+    public GameObject spawnPoint;
+   
+        void OnTriggerEnter (Collider col)
+        {
+            if ( gameObject.CompareTag( "myPlayer"))
+            {
+                this.transform.position = spawnPoint.transform.position;
+            }
+        }
+    }
+ 
