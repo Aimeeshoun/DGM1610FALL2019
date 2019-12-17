5204572f42d6753a5e47aaa4d0afb0be24857c3b
Aimee <10893245@my.uvu.edu>
2019-12-17 07:40:36 -0700

    RESPAWN SCRIPT IS WORKING! Coroutine Script

---
+++ Tutorial Practice/Assets/RespawnExperimenttwo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnExperimenttwo : MonoBehaviour
+{
+   
+    public GameObject myPlayer;
+    public Transform theRespawnPoint;
+   
+   
+    
+ 
+     void OnTriggerEnter(Collider other){
+        {
+            StartCoroutine(Respawn()); 
+        }
+    }
+ 
+    IEnumerator Respawn() {
+        Destroy(this.gameObject,5f);
+        yield return new WaitForSeconds(3f);
+        Instantiate (myPlayer, theRespawnPoint.position, Quaternion.identity);
+    }
+}
