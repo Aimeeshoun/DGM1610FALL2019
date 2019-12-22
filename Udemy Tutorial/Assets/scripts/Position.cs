38d7a8c2b27c9903739adc7fb3adac52a18018c9
Aimee <10893245@my.uvu.edu>
2019-12-21 18:34:08 -0700

    Udemy Tutorial Practice transition.position/translate

---
+++ Udemy Tutorial/Assets/scripts/Position.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Position : MonoBehaviour
+{
+    public float speed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.Translate(Vector3.right * speed*Time.deltaTime);
+
+        // Update is called once per frame
+        void Update()
+        {
+           
+        }
+    }
+}
