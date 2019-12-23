3ef1dd428015e47cc4944415b534394f267b59d9
Aimee <10893245@my.uvu.edu>
2019-12-23 13:14:55 -0700

    Got Movement Script Working!!!!

---
+++ Udemy Tutorial/Assets/scripts/Movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(0,7,6);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right*5*horizontalInput*Time.deltaTime);
+        
+    }
+}
